d51a077c633a333f5f96b76b0e02252299075204
KonvertPro <alfieharriswork@gmial.com>
2025-09-08 13:28:40 +0100

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
-  base: '/indra/',
+  base: '/',
 })
